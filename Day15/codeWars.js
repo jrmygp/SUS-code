5efdbf8f47b81d5ce03f2df3864a459f8d2e4494
jrmygp <fat.snorlax17@gmail.com>
2022-02-20 15:33:09 +0700

    got to 6kyu in codewars

--- Day15/codeWars.js
+++ Day15/codeWars.js
@@ -109,6 +109,277 @@
 //       }
 //   }
 //   return hasil.join("")
-  
+
 // };
 // console.log(fakeBin("123456"));
+
+// const getCount = (string) => {
+//     let vowels = ["a","i","u","e","o"]
+//     let stringArr = string.split("")
+//     let vowelcount = 0
+
+//     for (let i = 0; i < stringArr.length; i ++){
+//       if (vowels.includes(stringArr[i])) {
+//           vowelcount += 1
+//       }
+//     }
+//     return vowelcount
+//   }
+//   console.log(getCount("anjaygurinjay"))
+
+// const getMiddle = (string) => {
+//   let hasil = "";
+//   for (let i = 0; i < string.length; i++) {
+//     if (string.length % 2 == 0) {
+//       hasil = string.substring(string.length / 2 - 1, string.length / 2 + 1);
+//     } else if (!(string.length % 2 == 0)) {
+//       hasil = string.substring(string.length / 2, string.length / 2 + 1);
+//     }
+//   }
+//   return hasil;
+// };
+// console.log(getMiddle("Hello"));
+
+// const removeConsecutiveDuplicates = (string) => {
+//   let stringArr = string.split(" ");
+//   const removeDuplicate = (val,idx,arr) => {
+//       return arr.indexOf(val) == idx
+//   }
+//   const removed = stringArr.filter(removeDuplicate)
+//   const removedJoin = removed.join(" ")
+//   return removedJoin
+// };
+// console.log(removeConsecutiveDuplicates("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"))
+
+// const shortcut = (string) => {
+//     let vowels = ["a","i","u","e","o"]
+//     let stringArr = string.split("")
+//     let hasil = []
+//     for (let i = 0; i < stringArr.length; i++) {
+//       if (!vowels.includes(stringArr[i])){
+//         hasil.push(stringArr[i])
+//       }
+//     }
+//     hasil = hasil.join("")
+//     return hasil
+//   }
+// //   console.log(shortcut("anjaygurinjay"))
+
+// const helloWorld = () => {
+// return String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33)
+// from HTML ASCII Reference
+// }
+// console.log(helloWorld())
+
+// const squareDigits = (numb) => {
+//   let numbToString = String(numb);
+//   let numbArr = numbToString.split("")
+//   let splittedNumber = numbArr.map(Number)
+//   let hasil = []
+
+//   for (let i = 0; i < splittedNumber.length; i ++) {
+//     hasil.push(splittedNumber[i]*splittedNumber[i])
+//   }
+//   hasil = hasil.join("")
+//   hasil = Number(hasil)
+//   return hasil
+
+// };
+// console.log(squareDigits(1231))
+
+// const disemvowel = (string) => {
+//   let vowels = ["a", "i", "u", "e", "o"];
+//   let stringArr = string.split("");
+//   let hasil = [];
+//   for (let i = 0; i < stringArr.length; i++) {
+//     if (!vowels.includes(stringArr[i].toLowerCase())) {
+//       hasil.push(stringArr[i]);
+//     }
+//   }
+//   hasil = hasil.join("");
+//   return hasil;
+// };
+
+// console.log(disemvowel("Hello World LOL"))
+// const highAndLow = (string) => {
+
+//   let stringArr = string.split(" ");
+//   let largest = stringArr[0];
+//   let smallest = stringArr[0];
+//   for (let i = 0; i < stringArr.length; i++) {
+//       console.log(stringArr[i])
+//     if (largest < stringArr[i]) {
+//       largest = stringArr[i];
+//     } else if (stringArr[i] < smallest) {
+//       smallest = stringArr[i];
+//     }
+//   }
+//   return [largest, smallest].join(" ");
+
+// };
+// console.log(highAndLow("5 2 3 6 -1"));
+
+// const descendingOrder = (numb) => {
+//   let numbToString = String(numb);
+//   let stringArr = numbToString.split("").map(Number);
+//   stringArr.sort((a, b) => {
+//     if (a > b) {
+//       return 1;
+//     } else if (a < b) {
+//       return -1;
+//     }
+//     return 0;
+//   });
+//   return stringArr.join("");
+// };
+// //   let idk = [1,42,2,3,6,2,78]
+// console.log(descendingOrder(72628091));
+
+// const XO = (string) => {
+//   let stringArr = string.split("");
+//   let xCounter = 0;
+//   let oCounter = 0;
+//   for (let i = 0; i < stringArr.length; i++) {
+//     if (stringArr[i].toLowerCase() == "x") {
+//       xCounter += 1;
+//     } else if (stringArr[i].toLowerCase() == "o") {
+//       oCounter += 1;
+//     }
+//   }
+//   if (xCounter == oCounter) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(XO("xxOo"))
+
+// const findShort = (string) => {
+//   let stringArr = string.split(" ");
+//   let shortest = stringArr[0].length;
+//   for (let i = 0; i < stringArr.length; i++) {
+//     if (shortest > stringArr[i].length) {
+//       shortest = stringArr[i].length;
+//     }
+//   }
+//   return shortest;
+// };
+// console.log(
+//   findShort("aku adalah seorang lelaki yang tak pernah lelah mencari wanita")
+// );
+
+// const upArray = (arr = []) => {
+//   let copyOfArr = [...arr];
+//   let copyOfArr2 = [...arr];
+//   copyOfArr.splice(copyOfArr.length - 1, 1);
+//   let last = Number(copyOfArr.slice(copyOfArr.length - 1, copyOfArr.length));
+
+//   let lastNumber = copyOfArr2.pop();
+//   let added1 = lastNumber + 1;
+//   console.log(copyOfArr);
+
+//   if (added1 > 9) {
+//     added1 = added1 % 10;
+//     last = last + 1;
+//   } else if (added1 < 0) {
+//     return null;
+//   }
+//   copyOfArr.push(last);
+//   copyOfArr.push(added1);
+//   return copyOfArr;
+// };
+// let sample = [2, 3, 9];
+// console.log(upArray(sample));
+
+// const upArray = (arr = []) => {
+//   let lastNumber = Number(arr.slice(arr.length - 1, arr.length));
+//   let added1 = lastNumber + 1;
+//   copyOfArr = [...arr]; // disini arr menjadi [2,3,6]
+//   copyOfArr.splice(copyOfArr.length - 1, 1); // disini arr menjadi [2,3]
+
+//   if (added1 > 9) {
+//       added1 = added1 % 10
+//   } else if (added1 < 0) {
+//     return null;
+//   }
+//   copyOfArr.push(added1);
+//   return copyOfArr;
+// };
+// let sample = [2, 3, 6];
+// console.log(upArray(sample));
+//BELUM SELESAI
+
+// const toJadenCase = (string) => {
+//     let stringArr = string.split(" ")
+//     let hasil = ""
+//     for (let i = 0; i < stringArr.length; i++) {
+//         hasil += stringArr[i].toUpperCase()
+//     }
+//     return hasil
+//   }
+//   console.log(toJadenCase("babi ngepet lewat di depan rumahku"))
+// BELUM SELESAI
+
+// const friend = (arr = []) => {
+//   let hasil = []
+//   for (let i = 0; i < arr.length; i ++) {
+//       if (arr[i].length == 4) {
+//           hasil.push(arr[i])
+//       }
+//   }
+//   return hasil
+// }
+// let sample = ["Ryan", "Kieran", "Mark"]
+// console.log(friend(sample))
+
+// const solution = () => {
+//   maxNumber = 30;
+//   let sum = 0;
+//   for (let i = 0; i < maxNumber; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       sum += i;
+//     } else if (i % 3 == 0) {
+//       sum += i;
+//     } else if (i % 5 == 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+// console.log(solution());
+
+// const spinWords = (string) => {
+//   let stringArr = string.split(" ");
+//   let hasil = [];
+//   for (let i = 0; i < stringArr.length; i++) {
+//     if (stringArr[i].length < 5) {
+//       hasil.push(stringArr[i]);
+//     } else if (stringArr[i].length >= 5) {
+//       hasil.push(stringArr[i].split("").reverse().join(""));
+//     }
+// }
+//   return hasil.join(" ");
+// };
+// console.log(spinWords("apakah ada satu"));
+
+const likes = (names = []) => {
+  let hasil = ``;
+  for (let i = 0; i <= names.length; i++) {
+    if (names.length == 0) {
+      return (hasil = `no one likes this`);
+    } else if (names.length == 1) {
+      return (hasil = `${names[i]} likes this`);
+    } else if (names.length == 2) {
+      return (hasil = `${names[i]} and ${names[i + 1]} like this`);
+    } else if (names.length == 3) {
+      return (hasil = `${names[i]}, ${names[i + 1]} and ${names[i + 2]} like this`);
+    } else if (names.length >= 4) {
+      return (hasil = `${names[i]}, ${names[i + 1]} and ${names.length - 2} others like this`);
+    }
+  }
+  return hasil;
+};
+let sampleZero = []
+let sample = ["Peter", "Janice", "Max", "Robert", "Sully"];
+console.log(likes(sample));
